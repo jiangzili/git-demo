cf7993c44b8e813c638802baf9ff049b8807c3f5
jiangzili <1607631871@qq.com>
2018-06-12 09:24:20 +0800

    talk nothing

--- discuss.js
+++ discuss.js
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 窗前明月光，疑是地上霜。
+=======
+(function () {
+	function sayHi() {
+		console.log("Hi, git!");
+	}
+	sayHi();
+})();
+>>>>>>> 31facead0b9335eb3cedf59c8ba8b2a5f337ee44
---
+++ talk.java
@@ -0,0 +1,1 @@
+i have a dream,because i have a wife,so never give up.
